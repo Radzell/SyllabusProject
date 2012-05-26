5120caa980d35ade7817d4a4506907dc02efca72
Radzell Robinson <radzell@appmunki.com>
2012-05-26 14:22:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syllabusproject::Application.config.secret_token = '398580455796a04557291144fc7a1874d99e76256d09b5d117106bb524be2739ea968c0e1b12ad096c8854e8cc059b5d4b9eede8f40e8b0f43031b1d7cf06fe6'
